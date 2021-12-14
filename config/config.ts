cfc8d72cc073436f857046e6ad34512a661e9136
vighnesh153 <vighnesh.raut13@gmail.com>
2021-12-14 23:14:01 +0530

    chore: add some styles

---
+++ config/config.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Vighnesh Raut <me@vighnesh153.com>
+ */
+
+const config = {
+  navLinks: [
+    { name: 'About', url: '/#about' },
+    { name: 'Experience', url: '/#experience' },
+    { name: 'Projects', url: '/#projects' },
+    { name: 'Contact', url: '/#contact' },
+  ],
+};
+
+export default config;
