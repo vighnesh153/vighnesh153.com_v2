796cf270b995f26b53e1a2454abca71a0a40d4f4
vighnesh153 <vighnesh.raut13@gmail.com>
2021-12-28 10:04:33 +0530

    feat: implement mobile nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app';
 
-import 'styles/globals.css';
-import 'styles/Home.css';
-import 'styles/NavBar.css';
+import 'styles/globals.scss';
+import 'styles/Home.scss';
+import 'styles/NavBar.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
