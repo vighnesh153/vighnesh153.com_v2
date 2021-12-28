7c0b94dbd50c7d0db6503cfc4612489a029e84e0
vighnesh153 <vighnesh.raut13@gmail.com>
2021-12-28 13:25:33 +0530

    feat: implement desktop navigation

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 import Logo from 'icons/Logo';
 import config from '../config';
 
-const NavBar: React.FC = () => {
+const NavBar__Hamburger = (): JSX.Element => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   // Toggle the blur class on body based on active-ness of nav-link
@@ -22,7 +22,7 @@ const NavBar: React.FC = () => {
   };
 
   const hamburgerLine = (
-    <span className="header-menu__hamburger-btn-line standard-transition" />
+    <span className="header__hamburger-menu__hamburger-btn-line standard-transition" />
   );
 
   const toggleMenu = (to?: boolean) => () => {
@@ -30,60 +30,95 @@ const NavBar: React.FC = () => {
   };
 
   return (
-    <header>
-      <Link href="/">
-        <a className="header-menu__logo focus-dashed-outline">
-          <Logo />
-        </a>
-      </Link>
-      <div className="header__header-menu">
-        <button
-          className={clsx(
-            'header-menu__hamburger-btn',
-            navActiveClass,
-            'focus-dashed-outline'
-          )}
-          aria-label="Menu button"
-          onClick={toggleMenu()}
-        >
-          {hamburgerLine}
-          {hamburgerLine}
-          {hamburgerLine}
-        </button>
-        <aside
-          className={clsx(
-            'header-menu__side-bar',
-            navActiveClass,
-            'standard-transition'
-          )}
-          tabIndex={1}
-        >
-          <nav>
-            <ol className={clsx({ 't-600': menuOpen })}>
-              {config.navLinks.map(({ name, url }) => (
-                <li className="nav-link" key={name}>
-                  <a href={url} onClick={toggleMenu(false)}>
-                    {name}
-                  </a>
-                </li>
-              ))}
-            </ol>
-            <a href={config.resume.url} className="header-menu__resume">
-              {config.resume.name}
-            </a>
-          </nav>
-        </aside>
+    <div className="header__hamburger-menu only-below-768">
+      <button
+        className={clsx(
+          'header__hamburger-menu__hamburger-btn',
+          navActiveClass,
+          'focus-dashed-outline'
+        )}
+        aria-label="Menu button"
+        onClick={toggleMenu()}
+      >
+        {hamburgerLine}
+        {hamburgerLine}
+        {hamburgerLine}
+      </button>
+      <aside
+        className={clsx(
+          'header-menu__side-bar',
+          navActiveClass,
+          'standard-transition'
+        )}
+        tabIndex={1}
+      >
+        <nav>
+          <ol>
+            {config.navLinks.map(({ name, url }) => (
+              <li className="nav-link" key={name}>
+                <a
+                  className="fast-transition"
+                  href={url}
+                  onClick={toggleMenu(false)}
+                >
+                  {name}
+                </a>
+              </li>
+            ))}
+          </ol>
+          <a
+            href={config.resume.url}
+            className="header__resume fast-transition"
+          >
+            {config.resume.name}
+          </a>
+        </nav>
+      </aside>
 
-        <div
-          className={clsx(
-            'header-menu__backdrop',
-            navActiveClass,
-            'standard-transition'
-          )}
-          role="presentation"
-          onClick={toggleMenu(false)}
-        />
-      </div>
+      <div
+        className={clsx(
+          'header-menu__backdrop',
+          navActiveClass,
+          'standard-transition'
+        )}
+        role="presentation"
+        onClick={toggleMenu(false)}
+      />
+    </div>
+  );
+};
+
+const NavBar__Horizontal = (): JSX.Element => {
+  return (
+    <nav className="header__horizontal-menu at-and-above768">
+      <ol>
+        {config.navLinks.map(({ name, url }) => (
+          <li className="nav-link" key={name}>
+            <a href={url} className="fast-transition">
+              {name}
+            </a>
+          </li>
+        ))}
+      </ol>
+      <a href={config.resume.url} className="header__resume fast-transition">
+        {config.resume.name}
+      </a>
+    </nav>
+  );
+};
+
+const NavBar: React.FC = () => {
+  return (
+    <header>
+      <nav>
+        <Link href="/">
+          <a className="header-menu__logo focus-dashed-outline">
+            <Logo />
+          </a>
+        </Link>
+        <NavBar__Hamburger />
+        <NavBar__Horizontal />
+      </nav>
     </header>
   );
 };
