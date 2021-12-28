b77bf159052746c0355cec4f645d30ee7e05f923
vighnesh153 <vighnesh.raut13@gmail.com>
2021-12-28 20:22:16 +0530

    feat: implement about section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import 'styles/globals.scss';
 import 'styles/Home.scss';
 import 'styles/components/NavBar.scss';
 import 'styles/components/Introduction.scss';
+import 'styles/components/About.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import Anchor from 'components/Anchor';
 import NavBar from 'components/NavBar';
 import Introduction from 'components/Introduction';
+import About from 'components/About';
 
 const Home: NextPage = () => {
   return (
@@ -16,52 +17,7 @@ const Home: NextPage = () => {
 
       <main id="main" className="main">
         <Introduction />
-
-        <section id="about">
-          <h2 className="heading">
-            <span className="number">01.</span> About me
-          </h2>
-          <p>
-            Hello. My name is Vighnesh and I enjoy creating things that live on
-            the web. My interest in frontend development started a few years
-            back when I first learnt about HTML, CSS and Javascript. Since then,
-            I have been building teeny-tiny projects for the web for fun.
-            <br />
-            <br />
-            Fast-forward to today, and I have had the privilege to work for{' '}
-            <a href="https://amazon.com" target="_blank" rel="noreferrer">
-              a huge corporation
-            </a>
-            ,{' '}
-            <a href="https://smarter.codes/" target="_blank" rel="noreferrer">
-              an AI startup
-            </a>{' '}
-            and{' '}
-            <a href="https://www.tavisca.com/" target="_blank" rel="noreferrer">
-              a loyalty rewards company
-            </a>
-            .
-            <br />
-            <br />
-            My main focus these days is building is building accessible,
-            inclusive products and digital experiences at{' '}
-            <a href="https://amazon.com" target="_blank" rel="noreferrer">
-              Amazon
-            </a>{' '}
-            for the Appstore org.
-            <br />
-            <br />
-            Here are a few technologies I have been working with recently:
-            <br />
-            <br />
-            <ul>
-              <li>Javascript (ES6+)</li>
-              <li>Typescript</li>
-              <li>React</li>
-              <li>Nodejs</li>
-            </ul>
-          </p>
-        </section>
+        <About />
 
         <section id="experience">
           <h2 className="heading">
