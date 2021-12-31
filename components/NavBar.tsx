050584ba70ba8bf999c96d74cf00f76cd175a1ed
vighnesh153 <vighnesh.raut13@gmail.com>
2021-12-31 20:06:22 +0530

    feat: add more accessibility to navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,17 +2,119 @@
  * @author Vighnesh Raut <me@vighnesh153.com>
  */
 
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
 import Link from 'next/link';
 
 import Logo from 'icons/Logo';
 import config from 'config';
 import useWindowDimensions from 'hooks/useWindowDimensions';
+import { KeyCodes } from '../util';
 
 const NavBar__Hamburger = (): JSX.Element => {
   const [menuOpen, setMenuOpen] = useState(false);
   const { width } = useWindowDimensions();
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const navRef = useRef<HTMLElement>(null);
+
+  const removeTabFocus = {
+    'aria-hidden': true,
+    tabIndex: -1,
+  };
+
+  const menuFocusables = useRef<HTMLElement[] | null>(null);
+  const firstFocusableEl = useRef<HTMLElement | null>(null);
+  const lastFocusableEl = useRef<HTMLElement | null>(null);
+
+  const closeMenu = useCallback(
+    (withFocus?: boolean) => {
+      if (menuOpen && withFocus) {
+        buttonRef.current?.focus();
+      }
+      setMenuOpen(false);
+    },
+    [menuOpen]
+  );
+
+  const handleBackwardTab = useCallback(
+    (e: KeyboardEvent) => {
+      if (menuOpen && document.activeElement === firstFocusableEl.current) {
+        e.preventDefault();
+        lastFocusableEl.current?.focus();
+      }
+    },
+    [menuOpen]
+  );
+
+  const handleForwardTab = useCallback(
+    (e: KeyboardEvent) => {
+      if (menuOpen && document.activeElement === lastFocusableEl.current) {
+        e.preventDefault();
+        firstFocusableEl.current?.focus();
+      }
+    },
+    [menuOpen]
+  );
+
+  const onKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      switch (e.key) {
+        case KeyCodes.Escape:
+        case KeyCodes.EscapeIE11: {
+          closeMenu(true);
+          break;
+        }
+
+        // Tab key press
+        case KeyCodes.Tab: {
+          console.log('here');
+
+          if (menuFocusables.current?.length === 1) {
+            e.preventDefault();
+            break;
+          }
+
+          console.log(menuFocusables.current);
+
+          // Shift + Tab key press
+          if (e.shiftKey) {
+            handleBackwardTab(e);
+          }
+          // Only Tab key press
+          else {
+            handleForwardTab(e);
+          }
+
+          break;
+        }
+
+        default: {
+          break;
+        }
+      }
+    },
+    [handleBackwardTab, handleForwardTab, closeMenu]
+  );
+
+  const setFocusables = () => {
+    menuFocusables.current = [
+      ...(buttonRef.current ? [buttonRef.current] : []),
+      ...Array.from(navRef.current?.querySelectorAll('a') || []),
+    ];
+    firstFocusableEl.current = menuFocusables.current[0];
+    lastFocusableEl.current =
+      menuFocusables.current[menuFocusables.current.length - 1];
+  };
+
+  // store the focusable elements in refs for future use
+  useEffect(setFocusables, [width]);
+
+  // Register the key-down listeners
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyDown);
+    return () => document.removeEventListener('keydown', onKeyDown);
+  }, [onKeyDown]);
 
   // Toggle the blur class on body based on active-ness of nav-link
   useEffect(() => {
@@ -23,16 +125,21 @@ const NavBar__Hamburger = (): JSX.Element => {
   // close the open navigation menu.
   useEffect(() => {
     if (width >= 768) {
-      setMenuOpen(false);
+      closeMenu(false);
     }
-  }, [width]);
+  }, [width, closeMenu]);
 
   const navActiveClass = {
     active: menuOpen,
   };
 
   const hamburgerLine = (
-    <span className="header__hamburger-menu__hamburger-btn-line standard-transition" />
+    <span
+      className={clsx(
+        'header__hamburger-menu__hamburger-btn-line',
+        'standard-transition'
+      )}
+    />
   );
 
   const toggleMenu = (to?: boolean) => () => {
@@ -49,6 +156,7 @@ const NavBar__Hamburger = (): JSX.Element => {
         )}
         aria-label="Menu button"
         onClick={toggleMenu()}
+        ref={buttonRef}
       >
         {hamburgerLine}
         {hamburgerLine}
@@ -60,9 +168,8 @@ const NavBar__Hamburger = (): JSX.Element => {
           navActiveClass,
           'standard-transition'
         )}
-        tabIndex={1}
       >
-        <nav>
+        <nav ref={navRef}>
           <ol>
             {config.navLinks.map(({ name, url }) => (
               <li className="nav-link" key={name}>
@@ -70,13 +177,18 @@ const NavBar__Hamburger = (): JSX.Element => {
                   className="fast-transition"
                   href={url}
                   onClick={toggleMenu(false)}
+                  {...(menuOpen ? {} : removeTabFocus)}
                 >
                   {name}
                 </a>
               </li>
             ))}
           </ol>
-          <a href={config.resume.url} className="header__resume awesome-link">
+          <a
+            href={config.resume.url}
+            className="header__resume awesome-link"
+            {...(menuOpen ? {} : removeTabFocus)}
+          >
             {config.resume.name}
           </a>
         </nav>
