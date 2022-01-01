479a7ae7bcb9b628c3badd5494d8a885e6dc6eff
vighnesh153 <vighnesh.raut13@gmail.com>
2022-01-01 17:34:27 +0530

    refactor: distribute constants in different files

---
+++ config/base.tsx
@@ -0,0 +1,32 @@
+/**
+ * @author Vighnesh Raut <me@vighnesh153.com>
+ */
+
+export const companyUrls = {
+  Amazon: 'https://amazon.com',
+  SmarterCodes: 'https://smarter.codes',
+  Tavisca: 'https://tavisca.com',
+};
+
+export const smallConstants = {
+  myName: 'Vighnesh Raut',
+  emailAddress: 'me@vighnesh153.com',
+  githubRepoUrl: 'https://github.com/vighnesh153/vighnesh153.com_v2',
+  brittanyChiangWebsite: 'https://brittanychiang.com',
+  navLinks: [
+    { name: 'About', url: '/#about' },
+    { name: 'Experience', url: '/#experience' },
+    { name: 'Projects', url: '/#projects' },
+    { name: 'Contact', url: '/#contact' },
+  ],
+  resume: {
+    name: 'Resume',
+    url: 'https://bit.ly/vighnesh153-resume',
+  },
+  socialLinks: {
+    github: 'https://github.com/vighnesh153',
+    linkedIn: 'https://www.linkedin.com/in/vighnesh153',
+    codepen: 'https://codepen.io/vighnesh153',
+  },
+  skills: ['Javascript (ES6+)', 'Typescript', 'React', 'Nodejs'],
+};
---
+++ config/summary.tsx
@@ -0,0 +1,10 @@
+/**
+ * @author Vighnesh Raut <me@vighnesh153.com>
+ */
+
+import React from 'react';
+
+export const summary =
+  'I am a Software Engineer specializing in building exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products for different causes.';
+
+export const shortSummary = 'I build things, mostly for the web.';
