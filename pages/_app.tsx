257eedb9a429d45f538e58a09f1977935d884bd1
vighnesh153 <vighnesh.raut13@gmail.com>
2022-01-01 08:05:00 +0530

    refactor: projects component files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 import type { AppProps } from 'next/app';
 
+// Global styles
 import 'styles/globals.scss';
+
+// Home page styles
 import 'styles/Home.scss';
+
+// Navigation bar styles
 import 'styles/components/NavBar.scss';
+
+// Introduction component styles
 import 'styles/components/Introduction.scss';
+
+// About component styles
 import 'styles/components/About.scss';
+
+// Experience component styles
 import 'styles/components/Experience.scss';
+
+// Projects component styles
 import 'styles/components/Projects.scss';
+import 'styles/components/FeaturedProjects.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
