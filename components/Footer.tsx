1742909c6b94e64be2e704711ff618b04fb80dc2
vighnesh153 <vighnesh.raut13@gmail.com>
2022-01-02 20:05:37 +0530

    feat: add social links in footer below 768px

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,26 +9,42 @@ import Anchor from './Anchor';
 
 const Footer = (): JSX.Element => {
   return (
-    <footer className="footer">
-      Designed by{' '}
-      <Anchor
-        href={config.brittanyChiangWebsite}
-        openInNewTab
-        className="inline-link"
-      >
-        Brittany Chiang
-      </Anchor>{' '}
-      and built by{' '}
-      <Anchor href="/" className="inline-link">
-        Vighnesh Raut
-      </Anchor>
-      <br />
-      <Anchor
-        href={config.githubRepoUrl}
-        className="last-updated-on inline-link"
-      >
-        Last updated on: {new Date().toDateString()}
-      </Anchor>
+    <footer id="footer" className="footer">
+      <ul className="footer-social-links only-below-768">
+        {config.socialLinks.map(({ icon, url, title }) => (
+          <li key={url} className="footer-social-link">
+            <Anchor
+              openInNewTab
+              href={url}
+              aria-label={title}
+              className="focus-dashed-outline"
+            >
+              {icon}
+            </Anchor>
+          </li>
+        ))}
+      </ul>
+      <span>
+        Designed by{' '}
+        <Anchor
+          href={config.brittanyChiangWebsite}
+          openInNewTab
+          className="inline-link"
+        >
+          Brittany Chiang
+        </Anchor>{' '}
+        and built by{' '}
+        <Anchor href="/" className="inline-link">
+          Vighnesh Raut
+        </Anchor>
+        <br />
+        <Anchor
+          href={config.githubRepoUrl}
+          className="last-updated-on inline-link"
+        >
+          Last updated on: {new Date().toDateString()}
+        </Anchor>
+      </span>
     </footer>
   );
 };
