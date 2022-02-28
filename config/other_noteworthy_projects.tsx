6ef5241787f9eccfbcb11baa27abad86cd6baadf
vighnesh153 <vighnesh.raut13@gmail.com>
2022-02-28 08:47:37 +0530

    feat: add tsx-playground to featured and move useGlobalState to noteworthy

--- config/other_noteworthy_projects.tsx
+++ config/other_noteworthy_projects.tsx
@@ -6,6 +6,23 @@ import React from 'react';
 
 const otherNoteworthyProjects = [
   {
+    id: 'use-global-state',
+    heading: 'React useGlobalState library',
+    description: (
+      <>
+        A lightweight library that provides a React hook for having a piece of
+        global state. It is similar to the useState hook from the React library
+        but with the only addition that this hook{' '}
+        <strong className="highlight">persists</strong> the state.
+      </>
+    ),
+    technologies: ['React', 'Typescript', 'NPM', 'Github Actions'],
+    links: {
+      github: '//github.com/vighnesh153/react-use-global-state',
+      demo: '//www.npmjs.com/package/@vighnesh153/use-global-state',
+    },
+  },
+  {
     id: 'personal-blog',
     heading: 'Personal Blog',
     description:
