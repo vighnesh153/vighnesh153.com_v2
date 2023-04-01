8a3ad8d38bca3ebe89b2f44f592a6dda369382f5
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-04-01 18:17:56 +0530

    config: remove react-toolkit from noteworthy projects

--- config/other_noteworthy_projects.tsx
+++ config/other_noteworthy_projects.tsx
@@ -49,23 +49,23 @@ const otherNoteworthyProjects = [
       demo: '//blog.vighnesh153.com',
     },
   },
-  {
-    id: 'react-toolkit',
-    heading: 'React Toolkit',
-    description:
-      'A simple component library for my projects. It also has a bunch of common hooks that I use and other utility stuff',
-    technologies: [
-      'React',
-      'Typescript',
-      'Storybook',
-      'NPM',
-      'AWS S3 & Cloudfront',
-    ],
-    links: {
-      github: '//github.com/vighnesh153/react-toolkit',
-      demo: '//react-toolkit.vighnesh153.com',
-    },
-  },
+  // {
+  //   id: 'react-toolkit',
+  //   heading: 'React Toolkit',
+  //   description:
+  //     'A simple component library for my projects. It also has a bunch of common hooks that I use and other utility stuff',
+  //   technologies: [
+  //     'React',
+  //     'Typescript',
+  //     'Storybook',
+  //     'NPM',
+  //     'AWS S3 & Cloudfront',
+  //   ],
+  //   links: {
+  //     github: '//github.com/vighnesh153/react-toolkit',
+  //     demo: '//react-toolkit.vighnesh153.com',
+  //   },
+  // },
 ];
 
 export default otherNoteworthyProjects;
